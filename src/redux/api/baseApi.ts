449403d1e269197e29afb1a30226f2af02371ed6
Rika4698 <rikasharmin181@gmail.com>
2025-12-09 18:54:48 +0600

    create base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,11 +1,11 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-    // baseUrl: "https://eshop-ecommerce-application.vercel.app/api",
+    
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://eshop-ecommerce-application.vercel.app/api",
+    baseUrl: "http://localhost:5000/api/v1",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
